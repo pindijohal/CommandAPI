c7e19e465e795f78ecc4470803f24ddf5b6be8fe
Pindi Johal <pindi.johal@outlook.com>
2021-09-19 16:54:11 -0700

    Initial Commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+	[Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+		[HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            //var commandItems = _repository.GetAllCommands();
+
+            return new string[] {"this", "is", "hard", "coded"};
+        }
+	}
+}
