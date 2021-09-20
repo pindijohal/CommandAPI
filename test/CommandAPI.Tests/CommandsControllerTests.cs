f4e97663ae8cd4fd8b98712b1e77b39eb7d3a13e
Pindi Johal <pindi.johal@outlook.com>
2021-09-20 01:27:07 -0700

    Setup tests

---
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using AutoMapper;
+using CommandAPI.Models;
+using Xunit;
+using CommandAPI.Controllers;
+using CommandAPI.Data;
+using CommandAPI.Profiles;
+using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Dtos;
+
+namespace CommandAPI.Tests
+{
+	public class CommandsControllerTests
+	{
+		Mock<ICommandAPIRepo> mockRepo;
+		CommandsProfile realProfile;
+		MapperConfiguration configuration;
+		IMapper mapper;
+
+        public CommandsControllerTests()
+        {
+            mockRepo = new Mock<ICommandAPIRepo>();
+            realProfile = new CommandsProfile();
+            configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
+            mapper = new Mapper(configuration);
+        }
+
+        public void Dispose()
+        {
+            mockRepo = null;
+            mapper = null;
+            configuration = null;
+            realProfile = null;
+        }
+
+        [Fact]
+        public void GetAllCommands_ReturnsZeroResources_WhenDBIsEmpty()
+        {
+            //Arrange 
+            mockRepo.Setup(repo =>
+              repo.GetAllCommands()).Returns(GetCommands(0));
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            //Act
+            var result = controller.GetAllCommands();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        //**************************************************
+        //*
+        //Private Support Methods
+        //*
+        //**************************************************
+
+
+
+
+        private List<Command> GetCommands(int num)
+        {
+            var commands = new List<Command>();
+            if (num > 0)
+            {
+                commands.Add(new Command
+                {
+                    Id = 0,
+                    HowTo = "How to genrate a migration",
+                    CommandLine = "dotnet ef migrations add <Name of Migration>",
+                    Platform = ".Net Core EF"
+                });
+            }
+            return commands;
+        }
+    }
+}
